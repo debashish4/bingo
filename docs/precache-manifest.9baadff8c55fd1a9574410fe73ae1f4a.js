464467c44df1c2bfca3a738b25abec1fa55405f8
Debashish Sahoo <debashish4@gmail.com>
2018-11-06 00:27:16 +0530

    adding docs

---
+++ docs/precache-manifest.9baadff8c55fd1a9574410fe73ae1f4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23e1868b2c7a5d3808fd",
+    "url": "/static/js/main.23e1868b.chunk.js"
+  },
+  {
+    "revision": "d76d8e051e1acbdba80e",
+    "url": "/static/js/1.d76d8e05.chunk.js"
+  },
+  {
+    "revision": "23e1868b2c7a5d3808fd",
+    "url": "/static/css/main.599e2974.chunk.css"
+  },
+  {
+    "revision": "beaaae5390b75966ecbb936aa483ef34",
+    "url": "/index.html"
+  }
+];
